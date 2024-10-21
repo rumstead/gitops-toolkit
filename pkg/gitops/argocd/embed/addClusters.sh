433a75f34537442b9796d7505a5e8f97019f4dea
rumstead <37445536+rumstead@users.noreply.github.com>
2024-10-21 12:22:50 -0400

    fix(argoflags): updating how and were we apply argoflags
    Signed-off-by: rumstead <37445536+rumstead@users.noreply.github.com>

--- pkg/gitops/argocd/embed/addClusters.sh
+++ pkg/gitops/argocd/embed/addClusters.sh
@@ -8,7 +8,7 @@ CRI_GATEWAY="${CRI_GATEWAY:-"host.docker.internal"}"
 
 # login
 # https://docs.docker.com/desktop/networking/#i-want-to-connect-from-a-container-to-a-service-on-the-host
-argocd login "$CRI_GATEWAY:$ARGO_PORT" --plaintext $ARGOFLAGS --username "$ARGOUSER" --password "$ARGOPASSWD"
+argocd login "$CRI_GATEWAY:$ARGO_PORT" --skip-test-tls --username "$ARGOUSER" --password "$ARGOPASSWD" $ARGOFLAGS
 
 # don't quote $1 so it globs
-argocd cluster add -y --upsert "$CONTEXT" --plaintext $ARGOFLAGS --name "$CLUSTER" --kubeconfig "$KUBECONFIG" $1
+argocd cluster add -y --upsert "$CONTEXT" --name "$CLUSTER" --kubeconfig "$KUBECONFIG" $ARGOFLAGS $1
